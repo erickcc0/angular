6f6ba70afb6c7ac16c8e3f662d19820d6483f911
Erick Cornejo <c.cornejo.erick@gmail.com>
2021-11-07 22:56:34 -0500

    Lesson 1 finish

--- PP/01-angular/src/app/app.module.ts
+++ PP/01-angular/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { ContadorComponent } from './contador/contador/contador.component';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContadorComponent,
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeroesModule
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ PP/01-angular/src/app/contador/contador.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ContadorComponent } from './contador/contador.component';
+
+
+@NgModule({
+    declarations:[
+        ContadorComponent
+    ],
+    exports:[
+        ContadorComponent
+    ]
+})
+
+export class ContadorModule{
+
+}
